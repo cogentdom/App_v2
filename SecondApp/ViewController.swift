6e90860883c16fa713b5e7fdb981ae7a53d249fb
Dominik <cogentdom@gmail.com>
2021-04-06 13:02:09 -0600

    set pathing for login credentials

--- SecondApp/ViewController.swift
+++ SecondApp/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func validation(_ sender: Any) {
+        var uId = username.text!
+        var pass = password.text!
+        if (uId == "abc" && pass == "123") {
+            print("Welcome user ", uId)
+        }
+        else {
+            print("incorrect credentials")
+        }
+    }
 }
 
